05ba7157b7a0ccb9240a0f4f1163c5122e7fc405
Jeremy <jeremywoffinden25@gmail.com>
2017-12-05 22:43:51 -1000

    finished

--- Server.cpp
+++ Server.cpp
@@ -8,25 +8,25 @@
 #include <pthread.h>
 #include <iostream>
 #include <fstream>
+#include <sys/stat.h>
 #define PORT 8080
 
 using namespace std;
 
-void *handleMessage(char[]);
+void *handleMessage(void*);
+
+static int new_socket;
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
+    int server_fd;
     struct sockaddr_in address;
     int opt = 1;
     int num_threads = 64;
     int thread_count = 0;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
 
-    pthread_t *threads;
-    threads = (pthread_t*)calloc(num_threads,sizeof(pthread_t));
+    pthread_t threads[5];
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
@@ -55,30 +55,52 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
+    int threadCounter = 0;
+
+    while (threadCounter < 5){
+        cout << "Waiting for connection..." << endl;
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_create(&threads[threadCounter++], NULL, handleMessage, NULL);
     }
 
-    valread = read( new_socket , buffer, 1024);
-    printf("Text file: %s\n",buffer );
-    //char* copy = strdup(buffer);
-    // pthread_create(&threads[thread_count++], NULL, &handleMessage, (void*)buffer);
-    // for (int i = 0; i < num_threads; i++){
-	// 	pthread_join(threads[i],NULL);
-    // }
-    string line;
-    ifstream file (buffer);
-    while (getline(file, line)){
-        cout << line << endl;
+    for (int i = 0; i < 5; i++){
+        pthread_join(threads[i], NULL);
     }
-    file.close();
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
+    
+    
     return 0;
 }
 
-void *handleMessage(char c[]){
-    printf("%s", c);
+void *handleMessage(void* v){
+    char buffer[1024] = {0};
+    int valread;
+    int local_socket = new_socket;
+    char *hello = "Hello from server";
+    char *newline = "\n";
+    struct stat st;
+
+    valread = read( local_socket , buffer, 1024);
+    printf("Text file: %s\n",buffer );
+    string line;
+    ifstream file (buffer);
+    
+    while (getline(file, line)){
+        send(local_socket, line.c_str(), strlen(line.c_str()), 0);
+        send(local_socket, newline, strlen(newline), 0);
+    }
+    stat(buffer, &st);
+    int f = st.st_size;
+    printf("%d", f);
+    file.close();
+    string test = std::to_string(f);
+    send(local_socket, test.c_str(), strlen(test.c_str()), 0);
+    send(local_socket, newline, strlen(newline), 0);
+    send(local_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
 }
