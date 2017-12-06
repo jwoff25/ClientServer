eec0e1076b0e0272c19fdc813f014a1f386b8948
Jeremy <jeremywoffinden25@gmail.com>
2017-12-05 14:21:11 -1000

    working on file sending

--- Client.cpp
+++ Client.cpp
@@ -5,43 +5,41 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h> 
-#define PORT 8080
+#include <iostream>
 
 using namespace std;
   
 int main(int argc, char const *argv[])
 {
-    //
+    //variables
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    string filename;
+    char* msg;
     char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
+    int PORT;
+    
+    //prompt for port number
+    cout << "Please enter port number: ";
+    cin >> PORT;
+
+    cout << "Enter file name: ";
+    cin >> filename;
+    msg = (char*)filename.c_str();
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
   
     memset(&serv_addr, '0', sizeof(serv_addr));
   
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-      
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-  
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    send(sock , msg , strlen(msg) , 0 );
+    printf("Filename sent to server\n");
     valread = read( sock , buffer, 1024);
     printf("%s\n",buffer );
     return 0;
--- Server.cpp
+++ Server.cpp
@@ -4,37 +4,47 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h> 
+#include <semaphore.h>
+#include <pthread.h>
+#include <iostream>
+#include <fstream>
 #define PORT 8080
+
+using namespace std;
+
+void *handleMessage(char[]);
+
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
+    int num_threads = 64;
+    int thread_count = 0;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from server";
+
+    pthread_t *threads;
+    threads = (pthread_t*)calloc(num_threads,sizeof(pthread_t));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
       
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
       
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
       
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -44,15 +54,31 @@ int main(int argc, char const *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
     {
         perror("accept");
         exit(EXIT_FAILURE);
     }
+
     valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
+    printf("Text file: %s\n",buffer );
+    //char* copy = strdup(buffer);
+    // pthread_create(&threads[thread_count++], NULL, &handleMessage, (void*)buffer);
+    // for (int i = 0; i < num_threads; i++){
+	// 	pthread_join(threads[i],NULL);
+    // }
+    string line;
+    ifstream file (buffer);
+    while (getline(file, line)){
+        cout << line << endl;
+    }
+    file.close();
     send(new_socket , hello , strlen(hello) , 0 );
     printf("Hello message sent\n");
     return 0;
+}
+
+void *handleMessage(char c[]){
+    printf("%s", c);
 }
